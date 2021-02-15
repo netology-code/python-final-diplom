c79f1443913b04d58e0bc458f58d2dbcd64bbdf0
unknown <serge.rybakov@gmail.com>
2021-02-15 19:33:52 +0300

    CI check

--- web_shop/hello.py
+++ web_shop/hello.py
@@ -1,2 +1,12 @@
+"""Module for test."""
+
+
 def hello(name):
-    return f'Hello {name}'
+    """Test function.
+
+    Args:
+        name: some string
+    Returns:
+        another string
+    """
+    return f"Hello {name}"
--- web_shop/tests/test_hello.py
+++ web_shop/tests/test_hello.py
@@ -1,9 +1,17 @@
+"""Testing module."""
+
 import pytest
 from web_shop.hello import hello
 
-@pytest.mark.parametrize('name, result', (
-        ('world', 'Hello world'),
-        ('serge', 'Hello serge')
-))
+
+@pytest.mark.parametrize(("name", "result"), [("world", "Hello world"), ("serge", "Hello serge")])
 def test_hello(name, result):
+    """Test function.
+
+    Args:
+        name: string
+        result: string
+    Returns:
+        assertion
+    """
     assert hello(name) == result
