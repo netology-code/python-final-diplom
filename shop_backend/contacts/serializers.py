94aaeae95cbee7b0a3fdcfcdfcc33b27dd54bd3f
mamazu <mamazu91@gmail.com>
2022-01-01 16:49:09 +0300

    Forms initial commit

--- shop_backend/contacts/serializers.py
+++ shop_backend/contacts/serializers.py
@@ -1,41 +1,43 @@
 from rest_framework import serializers
-from .models import UserInfo
 from django.contrib.auth.models import User
+from .models import UserInfo
 from rest_framework.exceptions import ValidationError
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    middle_name = serializers.CharField(max_length=50)
-    company = serializers.CharField(max_length=50)
-    position = serializers.CharField(max_length=50)
+    middle_name = serializers.CharField(max_length=50, write_only=True)
+    company = serializers.CharField(max_length=50, write_only=True)
+    position = serializers.CharField(max_length=50, write_only=True)
     password_repeat = serializers.CharField(max_length=50, write_only=True)
 
     class Meta:
         model = User
         fields = ['first_name', 'middle_name', 'last_name', 'email', 'password', 'password_repeat', 'company',
                   'position']
-        write_only_fields = ['middle_name', 'company', 'position']
 
     def create(self, validated_data):
-        new_user, _ = User.objects.get_or_create(
+        new_user, is_new_user_created = User.objects.get_or_create(
             email=validated_data.get('email'),
             defaults={
                 'first_name': validated_data.get('first_name'),
                 'last_name': validated_data.get('last_name'),
+                'username': validated_data.get('email'),
                 'password': validated_data.get('password'),
             }
         )
 
-        new_user_info, _ = UserInfo.objects.update_or_create(
-            user_id=new_user.id,
-            defaults={
-                'middle_name': validated_data.get('middle_name'),
-                'company': validated_data.get('company'),
-                'position': validated_data.get('position')
-            }
-        )
+        if not is_new_user_created:
+            raise ValidationError({'email': ['User with this email already exists.']})
 
-        return new_user
+        new_user_info = UserInfo(
+            user_id=new_user.id,
+            middle_name=validated_data.get('middle_name'),
+            company=validated_data.get('company'),
+            position=validated_data.get('position')
+        )
+        new_user_info.save()
+
+        return validated_data
 
     def validate(self, data):
         password = data.get('password')
--- shop_backend/tests.py
+++ shop_backend/tests.py
@@ -1,33 +1,8 @@
-import yaml
 
 
-def price_list_to_yaml(price_list_filepath: str) -> dict:
-    try:
-        with open(price_list_filepath, mode='r', encoding='utf-8') as price_list_file:
-            try:
-                price_list = yaml.safe_load(price_list_file)
-                if not price_list:
-                    pass
-                # if not is_price_list_valid(price_list):
-                #     pass
-                return price_list
-            except yaml.YAMLError as yaml_load_exception:
-                yaml_load_error = yaml_load_exception.__dict__.get('problem')
-                pass
-    except FileNotFoundError:
-        pass
+def test(a, b):
+    return dict(zip(a, b))
 
-
-pl = price_list_to_yaml('test.yaml')
-
-#print(pl['goods'])
-
-for good in pl['goods']:
-    print(good)
-    print('parameters are here?')
-    print('parameters' in good)
-    print('parameters are empty?')
-    if good.get('parameters'):
-        print('no')
-    else:
-        print('yes')
+a = {'a': 1}
+b = {'b': 2}
+print(test(a, b))
