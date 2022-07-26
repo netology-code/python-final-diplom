1ad685200758e6a38e2dce801bc48247931aaa37
Radik Masagutov <radik121@mail.ru>
2022-07-26 17:04:51 +0300

    Add in_active for superuser

--- orders/custom_auth/models.py
+++ orders/custom_auth/models.py
@@ -38,11 +38,14 @@ class UserManager(BaseUserManager):
     def create_superuser(self, email, password, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
 
         if extra_fields.get('is_staff') is not True:
             raise ValueError('Superuser must have is_staff=True.')
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
+        if extra_fields.get('is_active') is not True:
+            raise ValueError('Superuser must have is_active=True.')
 
         return self._create_user(email, password, **extra_fields)
 
