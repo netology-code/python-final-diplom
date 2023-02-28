7de53ea36eff166b10ab096fda08b0a09bee15db
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-02-28 21:20:48 +0000

    added: EditUser

--- orders/api_urls.py
+++ orders/api_urls.py
@@ -23,10 +23,11 @@ urlpatterns = [
     path('partner/update', PartnerUpdate.as_view(), name='partner-update'),
     path('token/', obtain_auth_token),
 
+    path('shops', ShopView.as_view(), name='shop-list'),
+
     path('products/list', ProductsList.as_view(), name='products-list'),
     path('products/view', ProductsView.as_view(), name='products-view'),
     path('product/view_by_id', SingleProductView.as_view(), name='product-cart-view'),
-    path('shop/list', ShopView.as_view(), name='shop-list'),
     path('products/search', ProductInfoViewSet.as_view(), name='products'),
     path('basket', BasketView.as_view(), name='basket'),
     path('order', OrderView.as_view(), name='order'),
