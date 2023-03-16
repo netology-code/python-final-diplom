cdcf7c2713d21ae866760b1254a561da650ecb87
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-03-16 17:37:09 +0000

    added: test_user

--- commercial_net_service/settings.py
+++ commercial_net_service/settings.py
@@ -136,6 +136,7 @@ REST_FRAMEWORK = {
     # },
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 5,
+    #'TEST_REQUEST_DEFAULT_FORMAT': 'json',
 
 }
 
---
+++ tests/orders/test_user.py
@@ -0,0 +1,39 @@
+import pytest
+from rest_framework.test import APIClient
+
+from orders.models import User
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def user_data():
+    return {
+        "first_name": "test",
+        "last_name": "test_last",
+        "email": "test@gmail.com",
+        "password": "adminadmin",
+        "company": "Test",
+        "position": "funcionario",
+        "user_type": "shop",
+    }
+
+
+@pytest.mark.django_db
+def test_create_user(client, user_data):
+    # Arrange
+    user_count = User.objects.count()
+
+    # Act
+    request = client.post(path='/api/v1/user/register',
+                          data=user_data)
+
+    # Assert
+    assert request.status_code == 200
+    assert request.json() == {
+        "Status": True,
+    }
+    assert User.objects.count() == user_count + 1
