f03de3e73c7dbb7e36ca8b634842d87d440966bd
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-03-17 17:32:06 +0000

    added: кфшдцфн

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn 'name-of-application.wsgi'
--- commercial_net_service/settings.py
+++ commercial_net_service/settings.py
@@ -172,3 +172,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
