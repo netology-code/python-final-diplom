f4bae088d72817d116112f76f6e87f915623d658
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-03-18 21:08:07 +0000

    deploy static

--- commercial_net_service/settings.py
+++ commercial_net_service/settings.py
@@ -175,6 +175,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE="whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
